d615fabb6ff1fb646fa6f72ae2919547dcf60ab1
maxthestranger <thestrangermax@gmail.com>
2021-11-10 15:03:29 +0300

    tailwind setup and restructure

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full flex justify-center items-center h-screen bg-blue-50">
+        <p className="text-center text-gray-900 font-bold text-5xl"/>
+    </div>
+  )
+}
